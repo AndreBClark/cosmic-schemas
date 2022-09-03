a27c01ed50df24885706a6ca51ca483d4dab888a
Andre <andre@cosmicdivision.dev>
2022-09-03 15:50:37 -0700

    chore: move ts files to `./src`

--- schemas/sanity-meta/src/index.ts
+++ schemas/sanity-meta/src/index.ts
@@ -3,7 +3,8 @@ import Documents from "./schemas/documents";
 import Objects from "./schemas/objects";
 import Queries from "./queries";
 
-export default metaSchemaTypes;
 export const { siteMeta } = Documents;
 export const { openGraph, manifest, locale } = Objects;
 export const { getSiteMeta } = Queries;
+
+export default metaSchemaTypes;
