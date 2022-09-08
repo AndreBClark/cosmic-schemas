95a0c3683b2901349b701a5916182f8410ab717e
Andre <andre@cosmicdivision.dev>
2022-09-07 19:31:34 -0700

    style: increase type declaration specificity

--- schemas/sanity-meta/src/queries/getSiteMeta.ts
+++ schemas/sanity-meta/src/queries/getSiteMeta.ts
@@ -28,59 +28,11 @@ const querySiteMeta = `
   }
 }
 `
-
-
 export default async function getSiteMeta(
-  client: SanityClientLike,
-  query: string = querySiteMeta,
-  mutation = "fetch"
-):Promise<Site> {
-  const site: Site = await client[mutation](query)
+  client: Query.SanityClientLike<Site>,
+  query: Query.Query = querySiteMeta,
+  mutation: string = "fetch"
+):Promise<Query.Site> {
+  const site: Site = await client[Symbol(mutation)](query)
   return site
-}
-
-interface Meta {
-  title: string
-  description?: string
-  lang?: string
-}
-interface Site extends Meta {
-  openGraph?: OpenGraph
-  manifest?: Webmanifest
-}
-interface SanityClientLike {
-  [key: string]: (arg0: string) => Promise<Site>;
-  fetch: (arg0: string) => Promise<Site>
-}
-export interface OpenGraph {
-  basic: {
-    title: string;
-    type: string;
-    image: string;
-    url: URL["href"];
-  },
-  optional?: {
-    audio?: URL["href"];
-    description?: string;
-    determiner?: string;
-    locale?: string;
-    localeAlternate?: Array<string>;
-    siteName?: string;
-    video?: URL["href"];
-  }
-}
-interface ObjectSchema {
-  name: string
-  title: string
-  type?: string
-  options?: {}
-}
-interface Field extends ObjectSchema {
-  fieldset?: string
-}
-
-interface Webmanifest extends ObjectSchema {
-  type: string
-  fieldsets?: ObjectSchema[]
-  fields: Field[]
 }
--- schemas/sanity-meta/src/schemas/objects/opengraph/basic.ts
+++ schemas/sanity-meta/src/schemas/objects/opengraph/basic.ts
@@ -5,7 +5,7 @@ export default {
     {
       type: 'string',
       title: 'Page Title',
-      name: 'ogTitle',
+      name: 'title',
       description:
         'Set the title Open Graph should use. In most situations, this should be different from the value of the title prop',
       validation: (Rule: Rule) => Rule.required(),
@@ -21,7 +21,7 @@ export default {
     {
       type: 'image',
       title: 'Image',
-      name: 'ogImage',
+      name: 'image',
       description:
         'URL of the image that should be used in social media previews. If you define this, you must define two other OG basic properties as well: title and type.',
       validation: (Rule: Rule) => Rule.required(),
